f4c9137e092b51473510ab2f1e95a465e6a41629
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2024-06-06 03:37:46 +0700

    fix: ccp path

--- src/lib/fabric-client/constant.js
+++ src/lib/fabric-client/constant.js
@@ -13,7 +13,7 @@ const orgConfig = {
     admin: 'admin',
     caHostName: 'ca.org2.example.com',
     affiliation: 'org2.department1',
-    ccpPath: 'connection-org2.json',
+    ccpPath: '../../../../fabric-network/test-network/organizations/peerOrganizations/org2.example.com/connection-org2.json',
 };
 
 const CHAINCODES = {
@@ -21,36 +21,6 @@ const CHAINCODES = {
     ACCESS_DECISION: 'access-decision-asset-transfer',
     CHANNEL_POLICY: 'channel-policy-asset-transfer',
 };
-
-// const orgConfig = {
-//     merchant: {
-//         name: ORGANIZATION.org1,
-//         mspId: 'Org1MSP',
-//         peers: ['peer0.org1.example.com'],
-//         admin: 'admin',
-//         caHostName: 'ca.org1.example.com',
-//         affiliation: 'org1.department1',
-//         ccpPath: 'connection-org1.json',
-//     },
-//     admin: {
-//         name: ORGANIZATION.org2,
-//         mspId: 'Org2MSP',
-//         peers: ['peer0.org2.example.com'],
-//         admin: 'admin',
-//         caHostName: 'ca.org2.example.com',
-//         affiliation: 'org2.department1',
-//         ccpPath: 'connection-org2.json',
-//     },
-//     payment_provider: {
-//         name: ORGANIZATION.org3,
-//         mspId: 'Org3MSP',
-//         peers: ['peer0.org3.example.com'],
-//         admin: 'admin',
-//         caHostName: 'ca.org3.example.com',
-//         affiliation: 'org3.department1',
-//         ccpPath: 'connection-org3.json',
-//     },
-// };
 
 module.exports = {
     WALLET_PATH,
